0f1c6fc009060b415f8228e28389beeb3396f184
Sofia Junell <keijudesign@gmail.com>
2021-09-12 10:44:22 +0200

    added all files first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -16,7 +17,8 @@ import { EditusersComponent } from './editusers/editusers.component';
     EditusersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// const users = [
+//   {id:1, name:"sofia"}
+// ]; 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
+
+
+  // name: string = '';
+
+  // setValue(): void {
+  //   this.name = 'Nancy';
+  // }
+  
+
+
+  // userfromchild:string = "user from child";
+
+  // methodInParent(event:any){
+  //   console.log('whats passed from event', event);
+  //   this.userfromchild = event;
+  // }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // clicked(): void{
+  //   console.log("you clicked me")
+  // }
+
+items = ["item1"];
+
+addItem(newItem: string) {
+  this.items.push(newItem);
+}
 
 }
